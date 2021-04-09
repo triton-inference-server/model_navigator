060e9904d01817446a0da73e262ec645de2c4d34
Jakub Kosek <jkosek@nvidia.com>
2021-04-09 20:12:20 +0200

    Bump version: 0.0.0 → 0.1.0

--- model_navigator/__init__.py
+++ model_navigator/__init__.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-__version__ = "0.0.0"
+__version__ = "0.1.0"
 
 try:
     from model_navigator.core import (  # noqa: F401
