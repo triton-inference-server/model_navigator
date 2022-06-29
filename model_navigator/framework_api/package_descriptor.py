632d4aa353117e0a8e1f721af2b044ffffd49a7a
Kacper Kleczewski <kkleczewski@nvidia.com>
2022-06-29 06:04:38 -0700

    update nav pkg version and changelog

--- model_navigator/framework_api/package_descriptor.py
+++ model_navigator/framework_api/package_descriptor.py
@@ -51,7 +51,7 @@ from model_navigator.framework_api.utils import (
 from model_navigator.model import Format
 from model_navigator.utils.environment import get_env, get_git_info
 
-NAV_PACKAGE_FORMAT_VERSION = "0.1.0"
+NAV_PACKAGE_FORMAT_VERSION = "0.1.1"
 
 
 class PackageDescriptor:
